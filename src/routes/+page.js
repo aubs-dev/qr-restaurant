695e04687393eea8f2c63bd9dce6e30a5b7fd377
A. Dominik <hello@aubs.dev>
2024-05-18 05:49:02 +0100

    Added names below item

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -3,8 +3,10 @@ import Papa from 'papaparse';
 export async function load({fetch}) {
   const response = await fetch('/images.csv');
   const csvText = await response.text();
-  // console.log(csvText);  // Check if the CSV content is fetched
   const result = Papa.parse(csvText, {header: true});
-  // console.log(result.data);  // Check parsed CSV data
-  return {images: result.data.map(row => row.filename)};
+
+  return {
+    filename: result.data.map(row => row.filename),
+    name: result.data.map(row => row.name),
+  };
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,27 @@
 </script>
 
 <div class="grid">
-    {#each data.images as image}
-        <img src={`/images/${image}`} alt={image} />
+    {#each data.filename as filename, index}
+        <div class="box">
+            <img src={`/images/${filename}`} alt="sushi" />
+            <h1>{data.name[index]}</h1>
+        </div>
     {/each}
 </div>
 
 <style>
+    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap");
+
+    .box {
+        background-color: #a6a6a6;
+    }
+
+    h1 {
+        font-family: "Noto Sans", sans-serif;
+        font-size: 16px;
+        text-align: center;
+    }
+
     .grid {
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
